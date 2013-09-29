81fe7ce09ed0240ce89b15ff70f36eb27ea337f6
Chris-Meyers <chris-meyers47@hotmail.com>
2013-09-29 15:15:59 -0700

    Changes for 1st live submittal

--- v3/htmlFrame.py
+++ v3/htmlFrame.py
@@ -1,5 +1,7 @@
 #  h t m l F r a m e . p y
 #
+#  Chris Meyers. 09/25/2013
+#
 # Holder for attributes to be applied to a template and
 # a simple function to apply attributes to template and
 # send it to the <div> for display
@@ -21,7 +23,10 @@ class HtmlFrame :
         self.template = template
         self.banner   = banner
         self.item1 = self.item2 = self.item3 = ""
-        
+
+    def makeEofPage(self) :
+        pass
+            
     def makeFrame (self,template=None) :
         if not template : template = self.template
         content = template % self.__dict__
